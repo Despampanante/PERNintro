87c7ba3d83d7618367953abad91be92c91a8802c
Despampanante <santiago@depascale.org>
2023-02-04 19:30:58 -0500

    Finished basic pern stack

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "1234",
+    password: "",
     host: "localhost",
     port: 5432,
     database: "perntodo"
-})
+});
 
 module.exports = pool;
